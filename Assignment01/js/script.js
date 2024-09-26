b0f273e7b9d0e1a200ddac7a328c597614b5f2ee
k-pucci <168466463+k-pucci@users.noreply.github.com>
2024-09-25 17:36:32 -0700

    Add files via upload

---
+++ Assignment01/js/script.js
@@ -0,0 +1,108 @@
+// STEP 1
+// let someMonth;
+// function theMonth() {}
+// let currentMonth;
+// let summerMonth;
+// let myLibraryFunction;
+
+// STEP 2
+// let numericLiteral = 5;
+// let stringLiteral = "Hello";
+// let booleanLiteral = true;
+// let nullLiteral = null;
+
+// STEP 3
+// let complexExpression1 = 5 * (10 + 3);
+// let complexExpression2 = Math.sin(Math.PI / 2) + Math.log(10);
+
+// STEP 4
+// let strFirstName;
+// let strLastName;
+// let strAddress;
+// let strCity;
+// let strState;
+// let strZipCode;
+// let intYourAge;
+// let strReferralSource;
+// let blnMayWeContactYou;
+
+// STEP 5
+// strFirstName = "John";
+// strLastName = "Doe";
+// strAddress = "123 Main St";
+// strCity = "Anytown";
+
+// STEP 6
+// let result = 5 + "10";
+// console.log(result);
+
+// STEP 7
+// let booleanString = true + "test";
+// console.log(booleanString);
+// let numberBoolean = 10 + true;
+// console.log(numberBoolean);
+
+// STEP 8
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+// console.log(someString);
+
+// STEP 9
+// let nullVar = null;
+// console.log(nullVar);
+
+// STEP 10
+// let undefinedVar;
+// console.log(undefinedVar);
+
+// STEP 11
+// console.log(typeof "Hello");
+// console.log(typeof 42);
+// console.log(typeof true);
+// console.log(typeof {});
+// console.log(typeof undefined);
+
+// STEP 12
+// alert("Hello " + "Zak Ruvalcaba" + ", welcome to the JavaScript class!");
+
+// STEP 13
+// let name = "Zak Ruvalcaba";
+// alert("Hello " + name + ", welcome to the JavaScript class!");
+
+// STEP 14
+// let course = "JavaScript";
+// alert("Hello " + name + ", welcome to the " + course + " class!");
+
+// STEP 15
+// alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+
+// STEP 16
+// name = prompt("Please enter your name:");
+
+// STEP 17
+// course = prompt("Please enter the name of the class you are taking:");
+
+// STEP 18
+// let x = 10;
+// let y = 20;
+// console.log(x + y);
+
+// STEP 19
+// x = 20;
+// x += 20;
+// console.log(x);
+
+// STEP 20
+// x = 20;
+// x *= 5;
+// console.log(x);
+
+// STEP 21
+// x = 20 % 3;
+// x /= 1;
+// console.log(x);
+
+// STEP 22
+// console.log((5 > 3) && (10 < 20));
+
+// STEP 23
+// console.log((5 < 3) || (10 > 20));
